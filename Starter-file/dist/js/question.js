7edb016fa3d3ea5b3e2599021e6748dcd5024f33
zakaria0912 <58331380+zakaria0912@users.noreply.github.com>
2020-05-10 19:23:16 +0000

    add the functions in script-js

--- Starter-file/dist/js/question.js
+++ Starter-file/dist/js/question.js
@@ -2,120 +2,125 @@ export class Question {
     constructor() {
         this.questions_fr = [
             {
-                question: "Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?",
+                question: "Quetion 1: Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?",
                 type: "radio",
-                answers: ["oui", "non"]
+                answers: ["oui", "non"],
+                response: "non",
+                if: {  }
             },
             {
-                question: "Quelle est votre température corporelle ?",
+                question: " Quetion 2: Quelle est votre température corporelle ?",
                 type: "number",
-                label: "degés"
+                label: "degés",
+                conditions: {
+
+                }
             },
             {
-                question: "Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?",
+                question: "Quetion 3: Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?",
                 type: "radio",
                 answers: ["oui", "non"]
             },
             {
-                question: "Avez-vous eu des courbatures inhabituelles au cours des derniers jours ?",
+                question: "Quetion 4: Avez-vous eu des courbatures inhabituelles au cours des derniers jours ?",
                 type: "radio",
                 answers: ["oui", "non"]
             },
             {
-                question: "Ces derniers jours, avez-vous un mal de gorge ?",
+                question: "Quetion 5: Ces derniers jours, avez-vous un mal de gorge ?",
                 type: "radio",
                 answers: ["oui", "non"]
             },
             {
-                question: "Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles mol les.",
+                question: "Quetion 6: Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles mol les.",
                 type: 'radio',
                 answers: ['Oui', 'Non']
         
             }, {
-                question: 'Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ?',
+                question: 'Quetion 7: Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ?',
                 type: 'radio',
                 answers: ["oui", "non"]
         
             },
             {
-                question: 'Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?',
+                question: 'Quetion 9: Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?',
                 type: 'radio',
                 answers: ["oui", "non"]
             },
             {
-                question: 'Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?',
+                question: 'Quetion 10: Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?',
                 type: 'radio',
                 answers: ["oui", "non"]
         
             },
             {
-                question: 'Actuellement, comment vous vous sentez ?',
+                question: 'Quetion 11: Actuellement, comment vous vous sentez ?',
                 type: 'radio',
                 answers: ['Bien', 'Assez bien', 'Fatigué(e)', 'Très fatigué']
         
             },
             {
-                question: 'Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique.',
+                question: 'Quetion 12: Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique.',
                 type: 'number',
                 label: 'ans',
             },
             {
-                question: 'Quel est votre poids ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.',
+                question: 'Quetion 13: Quel est votre poids ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.',
                 type: 'number',
                 label: 'kg',
             },
             {
-                question: 'Quelle est votre taille ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.',
+                question: 'Quetion 14: Quelle est votre taille ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.',
                 type: 'number',
                 label: 'cm'
         
             },
             {
-                question: 'Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?',
+                question: 'Quetion 14: Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?',
                 type: 'radio',
                 answers: ["oui", "non"]
         
             },
             {
-                question: 'Êtes-vous diabétique ?',
+                question: 'Quetion 15: Êtes-vous diabétique ?',
                 type: 'radio',
                 answers: ["oui", "non"]
         
             },
             {
-                question: 'Avez-vous ou avez-vous eu un cancer ?',
+                question: 'Quetion 16: Avez-vous ou avez-vous eu un cancer ?',
                 type: 'radio',
                 answers: ["oui", "non"]
             },
             {
-                question: 'Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?',
+                question: 'Quetion 17: Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?',
                 type: 'radio',
                 answers: ["oui", "non"]
         
             }, 
             {
-                question: 'Avez-vous une insuffisance rénale chronique dialysée ?',
+                question: 'Quetion 18: Avez-vous une insuffisance rénale chronique dialysée ?',
                 type: 'radio',
                 answers: ["oui", "non"]
             }, 
             {
-                question: 'Avez-vous une maladie chronique du foie ?',
+                question: 'Quetion 19: Avez-vous une maladie chronique du foie ?',
                 type: 'radio',
                 answers: ["oui", "non"]
             }, 
             {
-                question: 'Êtes-vous enceinte ?',
+                question: 'Quetion 20: Êtes-vous enceinte ?',
                 type: 'radio',
                 answers: ["oui", "non", "homme"]
             }, 
             {
-                question: 'Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?',
+                question: 'Quetion 21: Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?',
                 type: 'radio',
                 answers: ["oui", "non"]
                 
             }, 
             {
-                question: 'Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples: corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste nonexhaustive).',
+                question: 'Quetion 22: Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples: corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste nonexhaustive).',
                 type: 'radio',
                 answers: ["oui", "non"]
             }
--- Starter-file/dist/js/script.js
+++ Starter-file/dist/js/script.js
@@ -5,25 +5,29 @@ const suivant = document.getElementById('suivant');
 const precedent = document.getElementById('precedent');
 const question_instance = new Question()
 
-let section = document.querySelectorAll('.section');
-let question = document.querySelector('.question');
-
 function create_question(id) {
+  // Getting question object
   const question_object = question_instance.get_question_by_id(id);
 
+  // Creating header for the question
   let question_header = document.createElement('p');
   question_header.classList.add('prg-question');
   question_header.setAttribute('id', `Q${id}`)
   question_header.innerText = question_object.question;
 
+  // Preparing a form for the question
   let question_form = document.createElement('div');
   question_form.classList.add('les-inputs');
 
+  // If the question type was choices "radio" ...
   if (question_object.type == "radio") {
+    // Creating a form field
     let form_field = document.createElement('div');
     form_field.classList.add('inpt');
 
-    for(answer of question_object.answers) {
+    // For every possible answer ...
+    for(let answer of question_object.answers) {
+      // Creating a form input for the answer
       let form_input = document.createElement('input');
       Object.assign(form_input, {
         type: 'radio',
@@ -32,18 +36,25 @@ function create_question(id) {
         value: answer,
       })
 
+      // Creating a form label for the answer
       let form_label = document.createElement('label')
       form_label.setAttribute('for', answer)
       form_label.innerText = answer
 
+      // Append the last two parts into the field
       form_field.append(form_input, form_label)
     }
 
+    // Append the field in the question form
     question_form.appendChild(form_field)
   }
-  if (question_object.type == "number") {
-    question_form.classList.add('answer-inputs');
 
+  // If the question type was numerical "number" ...
+  if (question_object.type == "number") {
+    // Modifing the class of the question form
+    question_form.setAttribute('class', 'answer-inputs');
+
+    // Creating the form input
     let form_input = document.createElement('input');
     Object.assign(form_input, {
       type: 'number',
@@ -53,23 +64,79 @@ function create_question(id) {
       placeholder: question_object.label
     })
 
+    // Creating the form label
     let form_label = document.createElement('span')
     form_label.classList.add('input-span')
     form_label.innerText = question_object.label
 
+    // Appending the last two parts into the question form
     question_form.append(form_input, form_label)
   }
 
+  // Returning the parts of the question block
+  return [question_header, question_form];
 }
 
-demarer.addEventListener('click', () =>{
+demarer.addEventListener('click', (event) =>{
+  event.preventDefault()
+
+  let question_container = document.querySelector('.question-affiche');
+  let section = document.querySelectorAll('.section');
+
   section[0].classList.add('hide');
   section[1].classList.remove('hide');
   precedent.classList.add('hide');
-  question.innerText = question_object.question;
+  
+  let question_parts = create_question(1)
+  question_container.innerHTML = "";
+  question_parts.forEach( part => question_container.appendChild(part) )
 });
 
-suivant.addEventListener('click', () =>{
-  question.innerHTML = questions[1];
+function quitter() {
+  let section = document.querySelectorAll('.section');
+  section[1].classList.add('hide');
+  section[2].classList.remove('hide');
+}
+
+function recommancer() {
+  let section = document.querySelectorAll('.section');
+  section[0].classList.remove('hide');
+  section[1].classList.add('hide');
+}
+
+suivant.addEventListener('click', (event) =>{
+  event.preventDefault()
+
+  let question_container = document.querySelector('.question-affiche');
+  let question_header = document.querySelector('.prg-question');
+  let question_id = parseInt(question_header.getAttribute('id')[1])
+
+  if (question_id == question_instance.get_questions_length()){
+    quitter();
+  } else {
+    let question_parts = create_question(question_id + 1)
+    
+    question_container.innerHTML = "";
+    question_parts.forEach( part => question_container.appendChild(part) )
+  }
+  
+  precedent.classList.remove('hide');
+});
+
+precedent.addEventListener('click', (event) =>{
+  event.preventDefault()
+
+  let question_container = document.querySelector('.question-affiche');
+  let question_header = document.querySelector('.prg-question');
+  let question_id = parseInt(question_header.getAttribute('id')[1])
+  
+  if (question_id-1 == 0){ recommancer(); } 
+  else {
+    let question_parts = create_question(question_id - 1)
+    
+    question_container.innerHTML = "";
+    question_parts.forEach( part => question_container.appendChild(part) )
+  }
+  
   precedent.classList.remove('hide');
 });
